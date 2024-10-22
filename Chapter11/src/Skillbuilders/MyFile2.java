b84e18beb67951bce7f2d2bc936b75ca324112fe
27346001 <27346001@L815WD220034.edu.cbe.ab.ca>
2024-10-22 15:32:20 -0600

    Oct 22nd 3:32 PM

--- Chapter11/src/Skillbuilders/MyFile2.java
+++ Chapter11/src/Skillbuilders/MyFile2.java
@@ -1,55 +1,50 @@
 package Skillbuilders;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MyFile2 {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
 		File textFile;
-		Sanner input = new Scanner(System.in);
-		String response;
-
-		textFile = new File("../Chapter11b/src/SkillBuilders/test1.Txt
-
+		Scanner input = new Scanner(System.in);
+		String response = null;
+		String filename = "";
+		
 		//obtain file name from the user
-				System.out.println("Please enter a file name:");
-				filename = input.nextLine();
-				
-				//Determine if a file exists
-				textFile = new File(filename);
-				
-				if(textFile.exists())
-				{
-					System.out.println("File exists.");
-				}
-				else
-				{
-					try
-					{
-					
-				}
-				catch (IOException e) 
-					
-					
-					
-					
-					
-			if(response.equalsIgnoreCase("D"))
-			{
-				System.out.println("Would you like" + "to(K)keep or (Delete teh file");
-				response = input.next();
-				}
-			if(response.equalsIgnoreCase("D"))
-			{
-				
-			//Delete
+		System.out.println("Please enter a file name: ");
+		filename = input.next();
+		
+		textFile = new File("../Chapter11/src/SkillBuilders/" + filename + ".Txt");
+		
+		//File textFile = new File(filename);
+		
+		if(textFile.exists())
+		{
+			System.out.println("File exists.");
+			System.out.println("Would you like" + "to(K)keep or (D)elete the file");
+			response = input.next();
+			response = response.toLowerCase();
+			
+			if(response.equals("d"))
+			{		
 				if(textFile.delete())
 				{
+					textFile.delete();
 					System.out.println("File Deleted!");
 				}
 				
-				}
-			else 
 			}
+			else if (response.equals("k")) 
+			{
+				System.out.println("Keeping the file");
+			}
+		}
+		else 
+		{
+			System.out.println("File does not exist");
+		}
 	}
-
 }
