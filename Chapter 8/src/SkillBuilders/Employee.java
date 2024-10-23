b08d118c0a156401069c10abc1214434b8d60e73
27346001 <27346001@L815WD220034.edu.cbe.ab.ca>
2024-10-23 13:20:28 -0600

    Oct 23rd

---
+++ Chapter 8/src/SkillBuilders/Employee.java
@@ -0,0 +1,22 @@
+package SkillBuilders;
+
+public abstract class Employee {
+
+		private String firstName, lastName;
+		
+		public Employee(String fN, String lN ) 
+		{
+			firstName = fn;
+			lastNmae = lN;
+		}
+		
+		public String toString()
+		{
+			return( firstname + "" + lastName);
+		}
+		
+		abstract double pay(double period);
+
+	}
+
+
---
+++ Chapter 8/src/SkillBuilders/Manager.java
@@ -0,0 +1,34 @@
+package SkillBuilders;
+
+public class Manager extends Employee 
+{
+	private double yearlySalary;
+	
+	//Constructor method
+	public Manager(String fN, String lN, double s) 
+	{
+		super(fN, lN);
+		yearlySalary = s;
+	}
+	public double getYearlySalary()
+	{
+		return yearlySalary;
+	}
+	
+	//Return the manager pay for a specified period
+	public double pay(double weeks)
+	{
+		double payEarned;
+		
+		payEarned = yearlySalary / 52 * weeks; 
+		
+		return payEarned;
+	}
+	
+	//Return the employee name, title, and salary
+	
+	public String toString()
+	{
+		return(super.toString() + ", Manager" + "Salary:" + yearlySalary);
+	}
+}
