a58f4fef29e5fa8569f2a5f60465b38faf30d520
27346001 <27346001@L815WD220034.edu.cbe.ab.ca>
2024-11-04 13:54:42 -0700

    Nov 4th 1:54 PM

---
+++ Chapter11/src/Masterys/RandomPrint.java
@@ -0,0 +1,16 @@
+package Masterys;
+
+public class RandomPrint {
+
+	public static void  printMessage(String... Message) 
+	{
+		String FullMessage = "";
+		
+		for (String Mes : Message) 
+		{
+			FullMessage += Mes;
+		}
+		
+		System.out.println(FullMessage);
+	}
+}
--- Chapter11/src/Masterys/wordCount.java
+++ Chapter11/src/Masterys/wordCount.java
@@ -1,14 +1,96 @@
 package Masterys;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Scanner;
 public class wordCount {
 
 	public static void main(String[] args) 
 	{
+		
 		File textFile;
 		
-		textFile = newFile("../Chapter11b/src/Masterys/source.txt");
-	}
-	
+		textFile = new File("../Chapter11/src/Masterys/source.txt");
+		try 
+		{
+			//FileWriter textFileWrite = new FileWriter(textFile);
+			//BufferedWriter buffWrite = new BufferedWriter(textFileWrite);
+			
+			int WordCount = 0;
+			Scanner textFileRead = new Scanner(textFile);
+			
+			while (textFileRead.hasNext()) 
+			{
+				String Word = textFileRead.next();
+				int Spaces = 0;
+				
+				Word = Word.replace('-', ' ');
+				
+				for (Character Char2 : Word.toCharArray()) 
+				{
+					if (Char2 == ' ') 
+					{
+						Spaces++;
+					}
+				}
+				
+				System.out.println(Spaces);
+				
+				//Word = Word.replace('-', ' ');
+				System.out.print("word: " + Word + " | ");
+				
+				boolean OnAWord = false;
+				
+				String validChars = "qwertyuiopasdfghjklzxcvbnm \n";
+				
+				for (Character Char : Word.toCharArray()) 
+				{
+					boolean isValid = false;
+					
+					for (Character Valid : validChars.toCharArray()) 
+					{
+						if (Char == Valid) 
+						{
+							isValid = true;
+							break;
+						}
+					}
+					
+					
+					if (Char == ' ' || Char == '\n')
+					{
+						OnAWord = false;
+					}
 
+					if (isValid) 
+					{
+						if (!OnAWord) 
+						{
+							WordCount++;
+							OnAWord = true;
+						}
+					}
+				}
+				
+				System.out.println(WordCount);
+			}
+			
+			System.out.println("Word Count: " + WordCount);
+			
+			//textFileRead.close();
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.err.println("No file given");
+		}
+		catch (IOException e) 
+		{
+			System.err.println("Got error");
+		}
+		
+	}
+		
+	public static void wordcntandavglngth(String filePath) 
+	{
+		int numwords = 0;
+		int totalLength = 0;
+	}
 }
